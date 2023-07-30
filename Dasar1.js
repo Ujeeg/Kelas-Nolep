8e74e449ea943b4e688226e466ccd4fa3b827ea3
Ujeeg <fajaraarrahman11@gmail.com>
2023-07-31 01:03:24 +0700

    week1eps3

---
+++ Dasar1.js
@@ -0,0 +1,59 @@
+// Variable
+var a ="a" ;
+let b ="b" ;
+const c="c" ;
+
+// varible string, integer/number, boolean, array, object
+
+let nama = "fajar";
+let umur = "26";
+let apakahBenar = true;
+let uang = 500.5
+
+// operator matematika
+
+let angka1 = 10;
+let angka2 = 20;
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 / angka2);
+console.log(angka1 * angka2);
+console.log(angka1 % angka2);
+
+let penampungHasilOperatorArimatika = angka1 + angka2;
+console.log(penampungHasilOperatorArimatika)
+
+penampungHasilOperatorArimatika = angka1 - angka2;
+console.log(penampungHasilOperatorArimatika)
+
+penampungHasilOperatorArimatika = angka1 / angka2;
+console.log(penampungHasilOperatorArimatika)
+
+penampungHasilOperatorArimatika = angka1 * angka2;
+console.log(penampungHasilOperatorArimatika)
+
+penampungHasilOperatorArimatika = angka1 % angka2;
+console.log(penampungHasilOperatorArimatika)
+
+
+// True = 1 dan False = 0 
+penampungHasilOperatorArimatika = false * angka1;
+console.log(penampungHasilOperatorArimatika)
+
+penampungHasilOperatorArimatika = true * angka1;
+console.log(penampungHasilOperatorArimatika)
+
+// Cara baca dari kiri ke kanan
+// Atas ke bawah
+
+let NamaKucing = "Pompom";
+console.log(NamaKucing);
+NamaKucing = "Tino";
+console.log(NamaKucing);
+
+// String 
+
+let FirstName = "fajar";
+let LastName = "Arief"
+let FullName = (FirstName + " " + LastName)
+console.log(FullName)
